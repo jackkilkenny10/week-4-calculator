23f0fcaf516cead92910d8a5a3bb2075b2e8f434
jackkilkenny10 <G00401535@atu.ie>
2023-10-19 17:19:03 +0100

    added new class called calculator to add two numbers. Removed main class

---
+++ src/main/java/ie/atu/calculator.java
@@ -0,0 +1,26 @@
+package ie.atu;
+
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args){
+        add();
+    }
+4
+    public static void add()
+    {
+        System.out.println("please enter your first number : ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+            System.out.println("please enter your second number : ");
+            int secondNumber = inputs.nextInt();
+
+            int total = firstNumber + secondNumber;
+            System.out.println("the total is " + total);
+
+    }
+    public static void delete()
+    {
+
+    }
+}
